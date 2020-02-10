6684c25ae9d4d35a92afa1fd09f831182600865f
sl23 <seanli.1996@gmail.com>
2020-02-10 16:27:58 -0500

    merging local master w remote

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,34 @@
 import React from "react"
 import logo from "./logo.svg"
 import "./App.css"
+<<<<<<< HEAD
 import ItemComponent from "./components/ItemComponent"
+=======
+import Head from "./components/head"
+>>>>>>> 873216acca70b28e38ffbfb7de1efa1f7fa88266
 
 function App() {
   return (
     <div>
+<<<<<<< HEAD
       <ItemComponent />
+=======
+      <Head />
+      <header>
+        <img className="logo" src="./assets/logo/thlogo.png" />
+      </header>
+      <body>
+        <p className="sofia-pro">test 1</p>
+        <p className="sofia-pro-bold">test 2</p>
+        <p className="grease-pencil">test 3</p>
+      </body>
+      <footer>
+        <img
+          className="promotion"
+          src="./assets/promotion/nutella/nutellaPromo.jpg"
+        />
+      </footer>
+>>>>>>> 873216acca70b28e38ffbfb7de1efa1f7fa88266
     </div>
   )
 }
---
+++ client/src/components/head.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function Head() {
+  return (
+    <head>
+      <link rel="stylesheet" href="style/reset.css" />
+      <link rel="stylesheet" href="style/fonts/fonts.css" />
+    </head>
+  )
+}
+
+export default Head
