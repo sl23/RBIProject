22859e9efe51d801de7656e7647a496be56b3838
sl23 <seanli.1996@gmail.com>
2020-02-10 16:00:46 -0500

    Basic item display component & revised data structure

---
+++ client/src/components/MenuData.js
@@ -0,0 +1,33 @@
+const menuData = {
+  hotBeverages: [
+    {
+      name: "Coffee",
+      image: "/assets/products/beverages/hot/coffee.png",
+      size: [
+        {
+          name: "Small",
+          price: 1.54,
+          calories: 3
+        },
+        {
+          name: "Medium",
+          price: 1.76,
+          calories: 4
+        },
+        {
+          name: "Large",
+          price: 1.92,
+          calories: 5
+        },
+        {
+          name: "Extra Large",
+          price: 2.19,
+          calories: 5
+        }
+      ],
+      blend: ["Original", "Dark Roast", "Decaf"]
+    }
+  ]
+}
+
+export default menuData
