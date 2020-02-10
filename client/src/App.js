ce59583a21c2fa753b3b015c3917bf863b163b17
azhaorbi <azhao@timhortons.com>
2020-02-10 16:33:21 -0500

    added promotion page link

--- client/src/App.js
+++ client/src/App.js
@@ -2,26 +2,45 @@ import React from "react"
 import logo from "./logo.svg"
 import "./App.css"
 import Head from "./components/head"
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom"
+import PromoPage from "./components/PromoPage"
 
 function App() {
   return (
-    <div>
-      <Head />
-      <header>
-        <img className="logo" src="./assets/logo/thlogo.png" />
-      </header>
-      <body>
-        <p className="sofia-pro">test 1</p>
-        <p className="sofia-pro-bold">test 2</p>
-        <p className="grease-pencil">test 3</p>
-      </body>
-      <footer>
-        <img
-          className="promotion"
-          src="./assets/promotion/nutella/nutellaPromo.jpg"
-        />
-      </footer>
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/">
+            <Head />
+            <header>
+              <img className="logo" src="./assets/logo/thlogo.png" />
+            </header>
+            <body>
+              <p className="sofia-pro">test 1</p>
+              <p className="sofia-pro-bold">test 2</p>
+              <p className="grease-pencil">test 3</p>
+            </body>
+            <Link to="/promotion">
+              <footer>
+                <img
+                  className="promotion"
+                  src="./assets/promotion/nutella/nutellaPromo.jpg"
+                />
+              </footer>
+            </Link>
+          </Route>
+          <Route path="/promotion">
+            <PromoPage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   )
 }
 
