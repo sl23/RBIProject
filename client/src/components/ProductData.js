ae35e48d0169e53b3444ea403172e603b87612c9
KatherineMcT <kmctaggart@rbi.com>
2020-02-10 13:40:52 -0500

    added hot beverages

--- client/src/components/ProductData.js
+++ client/src/components/ProductData.js
@@ -30,6 +30,58 @@ const menuData = {
         darkroast: "Dark Roast",
         decaf: "Decaf"
       }
+    },
+    tea: {
+      name: "Steeped Tea",
+      image: "/public/assets/products/beverages/hot/latte.png",
+      size: {
+        small: {
+          name: "SM",
+          price: 1.54,
+          calories: 0
+        },
+        medium: {
+          name: "MD",
+          price: 1.76,
+          calories: 0
+        },
+        large: {
+          name: "LG",
+          price: 1.92,
+          calories: 0
+        },
+        xlarge: {
+          name: "XL",
+          price: 2.19,
+          calories: 0
+        }
+      }
+    },
+    frenchVanilla: {
+      name: "French Vanilla",
+      image: "/public/assets/products/beverages/hot/frenchvanilla.png",
+      size: {
+        small: {
+          name: "SM",
+          price: 1.99,
+          calories: 243
+        },
+        medium: {
+          name: "MD",
+          price: 2.29,
+          calories: 309
+        },
+        large: {
+          name: "LG",
+          price: 2.59,
+          calories: 411
+        },
+        xlarge: {
+          name: "XL",
+          price: 2.79,
+          calories: 543
+        }
+      }
     }
   }
 }
