75e3c6d67cbf8f64a443982eeade4ce33b3653dc
aj-jha <abhishek.jha.06@gmail.com>
2020-02-11 14:03:35 -0500

    ColdBev in MenuData

--- client/src/components/MenuData.js
+++ client/src/components/MenuData.js
@@ -27,6 +27,64 @@ const menuData = {
       ],
       blend: ["Original", "Dark Roast", "Decaf"]
     }
+  ],
+
+  coldBeverages: [
+    {
+      name: "Iced Coffee",
+      image: "/assets/products/beverages/cold/Iced Latte Resized.png",
+      flavor: [
+        {
+          name: "Classic",
+          size: ["Small", "Medium", "Large"],
+          price: [1.79, 1.99, 2.49],
+          calories: [116, 151, 228]
+        }
+      ]
+    },
+
+    {
+      name: "Iced Capp",
+      image: "/assets/products/beverages/cold/ChocolateChipIcedCapp.png",
+      flavor: [
+        {
+          name: "Classic",
+          size: ["Small", "Medium", "Large"],
+          price: [2.59, 2.99, 3.79],
+          calories: [221, 305, 406]
+        },
+        {
+          name: "Light",
+          size: ["Small", "Medium", "Large"],
+          price: [2.59, 2.99, 3.79],
+          calories: [221, 305, 406]
+        }
+      ]
+    },
+    {
+      name: "Iced Capp Premium",
+      image: "/assets/products/beverages/cold/ChocolateChipIcedCapp.png",
+      flavor: [
+        {
+          name: "Vanilla",
+          size: ["Small", "Medium", "Large"],
+          price: [3.09, 3.79, 4.39],
+          calories: [351, 454, 574]
+        },
+        {
+          name: "Caramel",
+          size: ["Small", "Medium", "Large"],
+          price: [3.09, 3.79, 4.39],
+          calories: [356, 460, 581]
+        },
+        {
+          name: "Mocha",
+          size: ["Small", "Medium", "Large"],
+          price: [3.09, 3.79, 4.39],
+          calories: [353, 458, 579]
+        }
+      ]
+    }
   ]
 }
 
