105447281a790082aea4db36ecf8d20432285260
aj-jha <abhishek.jha.06@gmail.com>
2020-02-11 16:24:24 -0500

    ColdBev in MenuData v2

--- client/src/components/MenuData.js
+++ client/src/components/MenuData.js
@@ -3,26 +3,15 @@ const menuData = {
     {
       name: "Coffee",
       image: "/assets/products/beverages/hot/coffee.png",
-      size: [
+      options: [
         {
-          name: "Small",
-          price: 1.54,
-          calories: 3
-        },
-        {
-          name: "Medium",
-          price: 1.76,
-          calories: 4
-        },
-        {
-          name: "Large",
-          price: 1.92,
-          calories: 5
-        },
-        {
-          name: "Extra Large",
-          price: 2.19,
-          calories: 5
+          name: "Original Blend",
+          details: [
+            { size: "Small", price: 1.54, calories: 3 },
+            { name: "Medium", price: 1.76, calories: 4 },
+            { name: "Large", price: 1.92, calories: 5 },
+            { name: "Extra Large", price: 2.19, calories: 5 }
+          ]
         }
       ],
       blend: ["Original", "Dark Roast", "Decaf"]
@@ -33,55 +22,55 @@ const menuData = {
     {
       name: "Iced Coffee",
       image: "/assets/products/beverages/cold/Iced Latte Resized.png",
-      flavor: [
-        {
-          name: "Classic",
-          size: ["Small", "Medium", "Large"],
-          price: [1.79, 1.99, 2.49],
-          calories: [116, 151, 228]
-        }
+      options: [
+        { size: "Small", price: 1.79, calories: 116 },
+        { size: "Medium", price: 1.99, calories: 151 },
+        { size: "Large", price: 2.49, calories: 228 }
       ]
     },
 
     {
       name: "Iced Capp",
       image: "/assets/products/beverages/cold/ChocolateChipIcedCapp.png",
-      flavor: [
+      options: [
         {
           name: "Classic",
-          size: ["Small", "Medium", "Large"],
-          price: [2.59, 2.99, 3.79],
-          calories: [221, 305, 406]
-        },
-        {
-          name: "Light",
-          size: ["Small", "Medium", "Large"],
-          price: [2.59, 2.99, 3.79],
-          calories: [221, 305, 406]
+          details: [
+            { size: "Small", price: 2.59, calories: 221 },
+            { size: "Medium", price: 2.99, calories: 305 },
+            { size: "Large", price: 3.79, calories: 406 }
+          ]
         }
       ]
     },
+
     {
       name: "Iced Capp Premium",
       image: "/assets/products/beverages/cold/ChocolateChipIcedCapp.png",
-      flavor: [
+      options: [
         {
           name: "Vanilla",
-          size: ["Small", "Medium", "Large"],
-          price: [3.09, 3.79, 4.39],
-          calories: [351, 454, 574]
+          details: [
+            { size: "Small", price: 3.09, calories: 351 },
+            { size: "Medium", price: 3.79, calories: 454 },
+            { size: "Large", price: 4.39, calories: 574 }
+          ]
         },
         {
           name: "Caramel",
-          size: ["Small", "Medium", "Large"],
-          price: [3.09, 3.79, 4.39],
-          calories: [356, 460, 581]
+          details: [
+            { size: "Small", price: 3.09, calories: 356 },
+            { size: "Medium", price: 3.79, calories: 460 },
+            { size: "Large", price: 4.39, calories: 581 }
+          ]
         },
         {
           name: "Mocha",
-          size: ["Small", "Medium", "Large"],
-          price: [3.09, 3.79, 4.39],
-          calories: [353, 458, 579]
+          details: [
+            { size: "Small", price: 3.09, calories: 353 },
+            { size: "Medium", price: 3.79, calories: 458 },
+            { size: "Large", price: 4.39, calories: 579 }
+          ]
         }
       ]
     }
