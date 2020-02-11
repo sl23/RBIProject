74f823cb9b70e53535f4aa7d484ea4a51b1a6e4d
KatherineMcT <kmctaggart@rbi.com>
2020-02-11 14:01:58 -0500

    changed the hot beverage data

--- client/src/components/MenuData.js
+++ client/src/components/MenuData.js
@@ -26,6 +26,58 @@ const menuData = {
         }
       ],
       blend: ["Original", "Dark Roast", "Decaf"]
+    },
+    {
+      name: "Tea",
+      image: "/assets/products/beverages/hot/latte.png",
+      size: [
+        {
+          name: "Small",
+          price: 1.54,
+          calories: 0
+        },
+        {
+          name: "Medium",
+          price: 1.76,
+          calories: 0
+        },
+        {
+          name: "Large",
+          price: 1.92,
+          calories: 0
+        },
+        {
+          name: "Extra Large",
+          price: 2.19,
+          calories: 0
+        }
+      ]
+    },
+    {
+      name: "French Vanilla",
+      image: "client/public/assets/products/beverages/hot/frenchvanilla.png",
+      size: [
+        {
+          name: "Small",
+          price: 1.99,
+          calories: 243
+        },
+        {
+          name: "Medium",
+          price: 2.29,
+          calories: 309
+        },
+        {
+          name: "Large",
+          price: 2.59,
+          calories: 411
+        },
+        {
+          name: "Extra Large",
+          price: 2.79,
+          calories: 543
+        }
+      ]
     }
   ]
 }
