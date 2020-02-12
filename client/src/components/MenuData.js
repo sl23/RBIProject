b7a81f953ff9196e0ecc2e7219b445a99fc4352d
aj-jha <abhishek.jha.06@gmail.com>
2020-02-12 11:27:51 -0500

    test Lunch Data

--- client/src/components/MenuData.js
+++ client/src/components/MenuData.js
@@ -91,6 +91,62 @@ const menuData = {
         }
       ]
     }
+  ],
+
+  lunch: [
+    // {
+    //   name: "Iced Coffee",
+    //   image: "/assets/products/beverages/cold/Iced Latte Resized.png",
+    //   options: [
+    //     { size: "Small", price: 1.79, calories: 116 },
+    //     { size: "Medium", price: 1.99, calories: 151 },
+    //     { size: "Large", price: 2.49, calories: 228 }
+    //   ]
+    // },
+    // {
+    //   name: "Iced Capp",
+    //   image: "/assets/products/beverages/cold/ChocolateChipIcedCapp.png",
+    //   options: [
+    //     {
+    //       name: "Classic",
+    //       details: [
+    //         { size: "Small", price: 2.59, calories: 221 },
+    //         { size: "Medium", price: 2.99, calories: 305 },
+    //         { size: "Large", price: 3.79, calories: 406 }
+    //       ]
+    //     }
+    //   ]
+    // },
+    // {
+    //   name: "Iced Capp Premium",
+    //   image: "/assets/products/beverages/cold/ChocolateChipIcedCapp.png",
+    //   options: [
+    //     {
+    //       name: "Vanilla",
+    //       details: [
+    //         { size: "Small", price: 3.09, calories: 351 },
+    //         { size: "Medium", price: 3.79, calories: 454 },
+    //         { size: "Large", price: 4.39, calories: 574 }
+    //       ]
+    //     },
+    //     {
+    //       name: "Caramel",
+    //       details: [
+    //         { size: "Small", price: 3.09, calories: 356 },
+    //         { size: "Medium", price: 3.79, calories: 460 },
+    //         { size: "Large", price: 4.39, calories: 581 }
+    //       ]
+    //     },
+    //     {
+    //       name: "Mocha",
+    //       details: [
+    //         { size: "Small", price: 3.09, calories: 353 },
+    //         { size: "Medium", price: 3.79, calories: 458 },
+    //         { size: "Large", price: 4.39, calories: 579 }
+    //       ]
+    //     }
+    //   ]
+    // }
   ]
 }
 
