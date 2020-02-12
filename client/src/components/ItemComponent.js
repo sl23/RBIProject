12fbec53b3170f30c63ccd0f23441a8db54090b4
sl23 <seanli.1996@gmail.com>
2020-02-12 11:20:42 -0500

    WIP

--- client/src/components/ItemComponent.js
+++ client/src/components/ItemComponent.js
@@ -2,6 +2,7 @@ import React from "react"
 import menuData from "./MenuData"
 import CategoryDisplay from "./styledComponents/categoryDisplay"
 import ProductImg from "./ProductImage"
+import { object } from "prop-types"
 
 const ItemComponent = () => {
   // selects the menusection that is displayed. Can be modified to map over categories later on.
@@ -12,19 +13,35 @@ const ItemComponent = () => {
     <CategoryDisplay>
       {menuData[menuSection].map(products => {
         //code for displaying individual product section
+        console.log("Product Object: ", products)
         return (
           <div>
             <div>{products.name}</div>
             <ProductImg src={products.image} />
             <table>
               <tr>
-                <th>Blend</th>
-                {Object.values(products.size).map(size => {
-                  return <th>{size.name}</th>
+                <th>Options</th>
+                {products.options[0].details.map(detail => {
+                  console.log("options: ", detail)
+
+                  return <th>{detail.size}</th>
                 })}
               </tr>
-              {Object.values(products.blend).map(blend => {
-                console.log(blend)
+              <tr>
+                {Object.values(products.options).map(detail => {
+                  Object.values(detail).map(size => {
+                    console.log(size)
+                  })
+                  return (
+                    <div>
+                      <td>{detail.name}</td>
+                    </div>
+                  )
+                })}
+              </tr>
+              {console.log(products.options.detail)}
+              {/* {Object.values(products.options).map(blend => {
+                // console.log(blend)
                 return (
                   <tr>
                     <td>{blend}</td>
@@ -32,8 +49,7 @@ const ItemComponent = () => {
                       return <td>${size.price}</td>
                     })}
                   </tr>
-                )
-              })}
+              ) })} */}
             </table>
           </div>
         )
