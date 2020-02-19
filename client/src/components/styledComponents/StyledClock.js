06cae2d91dac80dd0dddb2dac9c871a0a50668ed
sl23 <seanli.1996@gmail.com>
2020-02-19 15:33:14 -0500

    styled on these n*ggas

---
+++ client/src/components/styledComponents/StyledClock.js
@@ -0,0 +1,9 @@
+import styled from "styled-components"
+
+const StyledClock = styled.div`
+  position: absolute;
+  top: 0px;
+  left: 0px;
+`
+
+export default StyledClock
--- client/src/components/styledComponents/StyledFooter.js
+++ client/src/components/styledComponents/StyledFooter.js
@@ -2,5 +2,6 @@ import styled from "styled-components"
 const StyledFooter = styled.footer`
   display: flex;
   justify-content: center;
+  margin: 30px;
 `
 export default StyledFooter
