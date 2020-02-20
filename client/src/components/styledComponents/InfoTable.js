cc770cd0940923fb3ca517f2a100d0ef45b2fd7d
aj-jha <abhishek.jha.06@gmail.com>
2020-02-20 16:12:55 -0500

    table styling tweak

--- client/src/components/styledComponents/InfoTable.js
+++ client/src/components/styledComponents/InfoTable.js
@@ -1,8 +1,9 @@
 import styled from "styled-components"
 
 const StyledTable = styled.table`
-  border-spacing: 2px;
+  border-spacing: 25px 2px;
   width: 100%;
+  word-wrap: break-word;
 `
 
 export default StyledTable
