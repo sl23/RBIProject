988b2d276c25cb872e10ecc42bb5fa5d66dd4dd3
sl23 <seanli.1996@gmail.com>
2020-02-21 15:39:54 -0500

    changed some icons

--- client/src/components/Greeting.js
+++ client/src/components/Greeting.js
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from "react"
 import Weather from "./Weather"
 import Clock from "./clock"
+import GreetingBackground from "./styledComponents/GreetingBackground"
+import day from "../assets/daypart/day.png"
 
 const Greeting = () => {
   return (
     <div>
       <Clock />
       <Weather />
+      <GreetingBackground src={day} />
     </div>
   )
 }
