1f5bb82273757534a3df34e2691b4e2b24abac7b
azhaorbi <azhao@timhortons.com>
2020-02-25 11:27:22 -0500

    deleted comments

--- client/src/components/Clock.js
+++ client/src/components/Clock.js
@@ -3,31 +3,6 @@ import GreasePencil from "./styledComponents/GreasePencil"
 import BrownSofiaPro from "./styledComponents/BrownSofiaPro"
 
 const time = new Date().toLocaleString()
-
-// class Clock extends React.Component {
-//   constructor(props) {
-//     super(props)
-//     this.state = {
-//       time: new Date().toLocaleString()
-//     }
-//   }
-//   componentDidMount() {
-//     this.intervalID = setInterval(() => this.tick(), 1000)
-//     // built in JS function that runs a function after the delay
-//   }
-//   componentWillUnmount() {
-//     clearInterval(this.intervalID)
-//   }
-//   tick() {
-//     this.setState({
-//       time: new Date().toLocaleString()
-//     })
-//   }
-//   render() {
-//     return <p className="App-clock">{this.state.time}.</p>
-//   }
-// }
-
 const Clock = ({ language }) => {
   const [time, setTime] = useState(new Date().toLocaleString())
   //setTime wil change time variable automatically.
