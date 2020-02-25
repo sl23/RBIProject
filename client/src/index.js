1c3585c5c99f3b135836433ef2d28335068eba4d
azhaorbi <azhao@timhortons.com>
2020-02-25 16:49:23 -0500

    fixing index.js'

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,6 @@ import "./index.css"
 import App from "./App"
 import * as serviceWorker from "./serviceWorker"
 import WebFont from "webfontloader"
-import "./style/fonts/fonts.css"
 
 WebFont.load({
   custom: {
