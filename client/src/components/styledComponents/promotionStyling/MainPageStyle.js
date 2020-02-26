2bcd3dd5b10a0c31da77f3085d634df813d495b7
aj-jha <abhishek.jha.06@gmail.com>
2020-02-26 11:15:34 -0500

    promo page styling changes

--- client/src/components/styledComponents/promotionStyling/MainPageStyle.js
+++ client/src/components/styledComponents/promotionStyling/MainPageStyle.js
@@ -1,8 +1,8 @@
 import styled from "styled-components"
 const MainPageStyle = styled.main`
-  overflow: hidden;
   display: grid;
   grid-template-columns: 1fr 1fr;
+  min-height: 100%;
   @media screen and (max-width: 1400px) {
     grid-template-columns: 1fr;
   }
