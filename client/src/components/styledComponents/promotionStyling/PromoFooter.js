43e651ad60b6aaf1eacc882fb8aee9ce5556ffb7
sl23 <seanli.1996@gmail.com>
2020-02-26 11:29:55 -0500

    added Current Promotions text

--- client/src/components/styledComponents/promotionStyling/PromoFooter.js
+++ client/src/components/styledComponents/promotionStyling/PromoFooter.js
@@ -2,7 +2,10 @@ import styled from "styled-components"
 const PromoFooter = styled.footer`
   display: flex;
   justify-content: center;
+  text-align: center;
   flex: 0 1 40px;
   color: #492a22;
+  margin-top: 30px;
+  margin-bottom: 30px;
 `
 export default PromoFooter
